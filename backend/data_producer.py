605bdad7f3121af7aec3f9689ccff193a01b58f3
verySpicyHeatWave <bncobb1989@gmail.com>
2024-10-17 15:29:41 -0700

    Revised the data structure in the data producer

--- backend/data_producer.py
+++ backend/data_producer.py
@@ -6,27 +6,37 @@ import time
 
 
 class DataProducer():
-    def __init__(self, instance_name: str = "None", minv: float = 3.0, maxv: float = 4.2):
+    def __init__(self, instance_name: str = "None", setv: float = 3.0):
         self.vals = {
-            "name" : instance_name,
-            "val1" : 0,
-            "val2" : 0,
-            "val3" : 0,
-            "val4" : 0,
-            "val5" : 0,
-            "val6" : 0
+            "battery_pn" : 0,
+            "battery_sn" : 0,
+            "pack_voltage" : 0,
+            "pack_current" : 0,
+            "cell1_voltage" : 0,
+            "cell2_voltage" : 0,
+            "cell3_voltage" : 0,
+            "cell4_voltage" : 0,
+            "cell5_voltage" : 0,
+            "cell6_voltage" : 0,
+            "cell7_voltage" : 0,
+            "cell8_voltage" : 0,
+            "pack_temp" : 0,
+            "bms_temp" : 0,
+            "cfet_temp" : 0,
+            "dfet_temp" : 0,
+            "board_temp" : 0,
+            "dpo" : False
         }
-        self.minv: float = minv
-        self.maxv: float = maxv
+        self.setv: float = setv
 
     def generate_data(self):
         for key in self.vals.keys():
             if key == "name": 
                 continue
-            self.vals[key] = round(((random.random() * (self.maxv - self.minv)) + self.minv), 3)
+            self.vals[key] = self.put_variation_on_cell_voltage(self.setv)
     
     def begin_stream(self, exchange_name='data'):
-        print(f"Beginning stream for {self.vals["name"]} on exchange {exchange_name}")
+        print(f"Beginning stream for {self.vals['battery_pn']} SN{self.vals['battery_sn']} on exchange {exchange_name}")
         connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
         channel = connection.channel()
 
@@ -44,6 +54,9 @@ class DataProducer():
         except KeyboardInterrupt:
             print(f"Killing stream for {self.vals["name"]} on exchange {exchange_name}")
             connection.close()
+
+    def put_variation_on_cell_voltage(self, voltage):
+        return round(((random.random() - 0.5) *.01 + voltage), 3)
             
 
 if __name__ == "__main__":
